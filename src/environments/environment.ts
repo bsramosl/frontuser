0208478296439da3bd33ad8e027e5072696d6236
bsramosl <steeven10f@gmail.com>
2024-01-09 23:01:16 -0500

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    endpoint: 'http://localhost:3000/'
+    production: true,
+    endpoint: 'https://server-production-9767.up.railway.app/'
 };
